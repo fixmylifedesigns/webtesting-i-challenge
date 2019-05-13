e7ecc8463bde975eb69761666bf83d46574b862f
Irving Duran <ijd.irving@gmail.com>
2019-05-13 18:07:45 -0300

    testing

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,15 +6,31 @@ module.exports = {
 };
 
 function succeed(item) {
-  return { ...item };
+  if (item.enhancment < 20) {
+   return { ...item, enhancment:item.enhancment + 1}; 
+  } else {
+    return { ...item, enhancment:item.enhancment}; 
+  }
 }
 
 function fail(item) {
-  return { ...item };
+  if ( item.enhancment < 15 && item.enhancment > 0) {
+    return { ...item, durability:item.durability - 5};
+  } else if (item.enhancment >= 15) {
+    return { ...item, durability:item.durability - 10};
+  } else if (item.enhancment > 16) {
+    return { ...item, enhancment:item.enhancment - 1}
+  } 
+  return item;
 }
 
 function repair(item) {
-  return { ...item };
+  if (item.durability < 100){
+    return { ...item, durability: 100 };
+  } else {
+   null;
+  }
+  
 }
 
 function get(item) {
